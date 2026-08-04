34309914853277e14e735830791bc415c0aa663e
Vinicius Almeida <viniciusalmeidadeavila@gmail.com>
2026-08-04 15:51:43 -0300

    commit

---
+++ Java/BlendPetBattleRoyalee/src/models/Usuario.java
@@ -0,0 +1,19 @@
+package models;
+
+public class Usuario {
+    private String nome;
+    private String senha;
+
+    public Usuario(String nome, String senha) {
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
--- Java/BlendPetBattleRoyalee/src/view/Main.java
+++ Java/BlendPetBattleRoyalee/src/view/Main.java
@@ -1,20 +1,30 @@
 package view;
 
+import models.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class Main{
+    private static List<Usuario> bancoDeDadosFake = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         int loop = -1;
         exibirTitulo();
-        System.out.println("Escolha uma opção");
+        System.out.print("Aperte ENTER para continuar...");
+        String enter = scanner.nextLine();
         while(loop != 0){
+            menu();
+            System.out.println("Escolha uma opção");
             int opcao = scanner.nextInt();
             switch (opcao){
                 case 1:
-                    menu();
                     break;
                 case 2:
+                    cadastrarUsuario();
                     break;
                 case 3:
                     break;
@@ -33,14 +43,38 @@ class Main{
     }
 
     public static void exibirTitulo() {
-        System.out.println("+-------------------------------------------------------------------------------+");
-        System.out.println("|  ____  _     _____ _   _ ____    ____  _____ _____                        |");
-        System.out.println("| | __ )| |   | ____| \\ | |  _ \\  |  _ \\| ____|_   _|                       |");
-        System.out.println("| |  _ \\| |   |  _| |  \\| | | | | | |_) |  _|   | |                         |");
-        System.out.println("| | |_) | |___| |___| |\\  | |_| | |  __/| |___  | |                         |");
-        System.out.println("| |____/|_____|_____|_| \\_|____/  |_|   |_____| |_|                         |");
-        System.out.println("|                                                                               |");
-        System.out.println("|               B A T T L E   R O Y A L E   E D I T I O N                       |");
-        System.out.println("+-------------------------------------------------------------------------------+");
+        System.out.println("===============================================================================");
+        System.out.println("                                                                               ");
+        System.out.println("  ██████╗ ██╗     ███████╗███╗   ██╗██████╗  ██████╗ ███████╗████████╗         ");
+        System.out.println("  ██╔══██╗██║     ██╔════╝████╗  ██║██╔══██╗ ██╔══██╗██╔════╝╚══██╔══╝         ");
+        System.out.println("  ██████╦╝██║     █████╗  ██╔██╗ ██║██║  ██║ ██████╔╝█████╗     ██║            ");
+        System.out.println("  ██╔══██╗██║     ██╔══╝  ██║╚██╗██║██║  ██║ ██╔═══╝ ██╔══╝     ██║            ");
+        System.out.println("  ██████╦╝███████╗███████╗██║ ╚████║██████╔╝ ██║     ███████╗   ██║            ");
+        System.out.println("  ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═══╝╚═════╝  ╚═╝     ╚══════╝   ╚═╝            ");
+        System.out.println("                                                                               ");
+        System.out.println("                 B A T T L E   R O Y A L E   E D I T I O N                     ");
+        System.out.println("                                                                               ");
+        System.out.println("===============================================================================");
+    }
+
+    public static void cadastrarUsuario() {
+
+        System.out.println("=======================================");
+        System.out.println("         CADASTRO DE USUÁRIO           ");
+        System.out.println("=======================================");
+
+        System.out.print("Digite o seu nome: ");
+        String nome = scanner.next();
+
+        System.out.print("Crie uma senha forte: ");
+        String senha = scanner.next();
+
+        Usuario novoUsuario = new Usuario(nome, senha);
+
+        bancoDeDadosFake.add(novoUsuario);
+
+        System.out.println("\n[+] Usuario(a) " + novoUsuario.getNome() + " cadastrado(a) com sucesso!");
+        System.out.println("Total de usuarios no sistema: " + bancoDeDadosFake.size());
+        System.out.println("=======================================\n");
     }
 }
